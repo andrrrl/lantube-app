752d522fb175e3f126348f4bb12a91f3356e574d
Andrrr <andresin@gmail.com>
2017-07-30 16:05:40 -0300

    Videos service

---
+++ src/app/services/videos.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class VideosService {
+
+  // TODO: MOVE TO PRIVATE CONFIG
+  private API = 'http://localhost:3000';
+
+  constructor(private http: Http) { }
+
+    extractData(res: Response) {
+      return res.json();
+    }
+
+    get(params: any): Observable<any[]> {
+      return this.http.get(this.API + '/api/videos', params).map(this.extractData);
+    }
+
+    getById(id: string): Observable<any> {
+      return this.http.get(this.API + '/api/videos/' + id).map(this.extractData);
+    }
+
+    play(id: String): Observable<any[]> {
+      return this.http.get(this.API + '/api/videos/' + id + '/play').map(this.extractData);
+    }
+
+    playAll(): Observable<any[]> {
+      return this.http.get(this.API + '/api/videos/playlist').map(this.extractData);
+    }
+
+    stopAll(): Observable<any[]> {
+      return this.http.get(this.API + '/api/videos/stop').map(this.extractData);
+    }
+
+    save(video: any): Observable<any> {
+      return this.http.post(this.API + '/api/videos/', video).map(this.extractData);
+    }
+
+    put(video: any): Observable<any> {
+      return video;
+    }
+}
