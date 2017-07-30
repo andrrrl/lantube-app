5350063b887fb2beb017c3dfa3dcfed591571b19
Andrrr <andresin@gmail.com>
2017-07-30 16:06:11 -0300

    Video list component

---
+++ src/app/lista-videos/lista-videos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-videos',
+  templateUrl: './lista-videos.component.html',
+  styleUrls: ['./lista-videos.component.css']
+})
+export class ListaVideosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
